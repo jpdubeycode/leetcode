b2d9d64d7dd984a62cf98f7a340dc0c1ee6e4a44
jpdubeycode <jaidubeyiitm@gmail.com>
2025-12-26 20:13:55 +0530

    mergesortedarray

---
+++ mergetwosortedarray.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+void mergesort(int a[],int n,int b[],int m,int c[]){
+
+int i=0,j=0,k=0;
+while(i<n&&j<m){
+    if(a[i]<b[j]){
+       c[k++]= a[i++];
+    }
+    else{
+       c[k++]= b[j++];
+    }
+}
+while(i<n){
+    c[k++]= a[i++];
+}
+while(j<m){
+    c[k++]= b[j++];
+}
+for(int x=0;x<k;x++){
+cout<<c[x]<<" ";
+}
+}
+
+int main(){
+
+int a[]={2,4,6,7,8};
+int b[]={3,5,9};
+int n,m;
+n=sizeof(a)/sizeof(a[0]);
+m=sizeof(b)/sizeof(b[0]);
+int c[8];
+mergesort(a,n,b,m,c);
+
+}
---
+++ plusone.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+int n;
+cin>>n;
+vector<int>v(n,0);
+n=v.size();
+for(int i=0;i<n;i++){
+ cin>>v[i];
+}
+
+v[n-1]++;
+for(int i=0;i<n;i++){
+    cout<<v[i];
+}
+
+
+}
